10768c9f62126080208af3b2283d5155935471d2
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2017-10-31 15:05:20 +0800

    Added Strongly Connected Components Generator.

--- src/sat/Graph.java
+++ src/sat/Graph.java
@@ -10,11 +10,14 @@ import sat.formula.Literal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Graph {
-	private HashMap<Literal, ArrayList<Literal>> adj = new HashMap<Literal, ArrayList<Literal>>(); // Adjacency list
-	private HashMap<Literal, Literal> parent = new HashMap<Literal,Literal>();
-	private ArrayList<Literal> V = new ArrayList<Literal>(); // All vertices in the graph
+	private HashMap<Literal, ArrayList<Literal>> adj = new HashMap<>(); // Adjacency list
+	private HashMap<Literal, Literal> parent = new HashMap<>();
+	private ArrayList<Literal> V, SCC = new ArrayList<>(); // All vertices in the graph
+	private Stack<Literal> S = new Stack<>();
+	private boolean satisfiable;
 
 	public Graph(Formula formula) {
 		for(Clause c : formula.getClauses()) {
@@ -25,6 +28,7 @@ public class Graph {
 			else if(c.size() == 0) {
 				// Trivial case: false
 				// TODO: Return false
+				satisfiable = false;
 			}
 			else if(c.size() == 1) {
 				Literal lit = c.chooseLiteral();
@@ -52,14 +56,14 @@ public class Graph {
 				ArrayList<Literal> firstLitAdj = adj.get(nFirstLit);
 				ArrayList<Literal> secondLitAdj = adj.get(nSecondLit);
 
-				// If the vertex has edges already, append. If it doens't, initiate the ArrayList
+				// If the vertex has edges already, append. If it doesn't, initiate the ArrayList
 				if(firstLitAdj == null) {
-					firstLitAdj = new ArrayList<Literal>();
+					firstLitAdj = new ArrayList<>();
 				}
 				firstLitAdj.add(secondLit);
 
 				if(secondLitAdj == null) {
-					secondLitAdj = new ArrayList<Literal>();
+					secondLitAdj = new ArrayList<>();
 				}
 				secondLitAdj.add(firstLit);
 
@@ -68,24 +72,80 @@ public class Graph {
 			}
 		}
 	}
+	
+	public Graph(ArrayList<Literal> V){
+		this.V = V;
+	}
 
-	public void DFS_visit(Literal s) {
+	public void DFS_visit(Graph graph, int counter, Literal s) {
+		SCC.add(counter, s);
 		ArrayList<Literal> adjVertices = this.adj.get(s);
 		for(Literal v : adjVertices) {
 			if(this.parent.get(v) == null) {
 				this.parent.put(v, s);
-				DFS_visit(v);
+				DFS_visit(graph, counter, v);
 			}
 		}
-	}
 
-	public void DFS() {
+	}
+	
+//	public void DFS_visitSCC(Literal s) {
+//		SCC.add(s);
+//		ArrayList<Literal> adjVertices = this.adj.get(s);
+//		for(Literal v : adjVertices) {
+//			if(this.parent.get(v) == null) {
+//				this.parent.put(v, s);
+//				DFS_visitSCC(v);
+//
+//			}
+//		}
+		
+//	}
+
+	public void DFS(Graph graph) {
 		for(Literal s : this.V) {
 			if(parent.get(s) == null) {
 				parent.put(s, null);
-				DFS_visit(s);
+				DFS_visit(graph,0, s);
+			}
+			S.push(s);
+		}
+		
+	}
+	
+	public ArrayList<Literal> getSCC(){ //Create Strongly Connected Component
+		this.DFS(this);
+		Graph transposedGraph = this.getTranspose();
+		HashMap<Literal, ArrayList<Literal>> scc = new HashMap<>();
+		//Clear SCC and Parent for second DFS
+		SCC.clear();
+		parent.clear();
+		int counter = 0;
+		while (!S.empty()){
+			Literal v = S.pop();
+			DFS_visit(transposedGraph, counter, v);
+			counter++;
+		}
+		return SCC;
+		
+	}
+	
+	public Graph getTranspose(){
+		Graph newGraph = new Graph(this.V);
+		for (Literal lit1 : this.V){
+			ArrayList<Literal> adjacency = this.adj.get(lit1);
+			for (Literal lit2 : adjacency){
+				ArrayList<Literal> lit2adj = newGraph.adj.get(lit2);
+				if ( lit2adj == null) {
+					lit2adj = new ArrayList<Literal>();
+					lit2adj.add(lit2);
+				}
+				else {
+					lit2adj.add(lit2);
+				}
 			}
 		}
+		return newGraph;
 	}
 
 	public void display() {
